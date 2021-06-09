647cca548f3ce828872783162969e51102975c4e
Md Muktadir <70088622+Strelitzia-02@users.noreply.github.com>
2021-06-10 03:08:20 +0600

    Add files via upload

---
+++ LAB-04/Source_codes/EXCERCISE_01/PointClass.java
@@ -0,0 +1,44 @@
+//EXCERCISE 1
+//using getter and setter methods for encapsulations
+public class Point {
+
+	private int x, y;
+	
+	public Point() {
+		this.x = 10;
+		this.y = 56;
+	}
+	
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public void setXY(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	
+	
+
+     public String String() {
+		return "(" + this.x + ", " + this.y +")";
+	}
+}
