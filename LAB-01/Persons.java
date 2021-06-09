621fd26095e8ab1e7178b13d3d62d08cd41c345e
Md Muktadir <70088622+Strelitzia-02@users.noreply.github.com>
2021-06-10 02:53:56 +0600

    Delete Persons.java

--- LAB-01/Persons.java
+++
@@ -1,9 +0,0 @@
-public class Persons{ //Here concept of constructor are used as express the abilities of Raiyan,Anika ,Tahsin ,Salman
-	protected String name, abilities;
-	protected Persons(String name , String abilities){
-		this.name = name ;
-		this.abilities = abilities ;
- 
-   }
-
-}
